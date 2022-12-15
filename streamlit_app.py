3823cf8af060107255d995e6a3f6604fc84c76c4
manideep_dyavanapalli <55358647+manideepdyavanapalli@users.noreply.github.com>
2022-12-15 13:49:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,28 +33,25 @@ except URLError as e:
   streamlit.error()
 #do not run anything past this
 
-
 streamlit.header("The fruit load list contains: ")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from fruit_load_list")
          return my_cur.fetchall()
-#Add a button to load the fruit
+
+ #Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cux.execute("insert into fruit_load_list values ('" + new_fruit +"')")
+         my_cux.execute("insert into fruit_load_list values ('" + add_my_fruit +"')")
          return "Thanks for adding" + new_fruit
-    
-streamlit.stop()
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','saporta')
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')");
 
